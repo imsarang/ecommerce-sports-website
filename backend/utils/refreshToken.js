5fa0c994636728cd2e800656770a0600c052b8e5
imsarang <84568747+imsarang@users.noreply.github.com>
2022-07-06 23:37:20 +0530

    clear git

--- backend/utils/refreshToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-exports.refreshToken = (id)=>{
-    return jwt.sign({id},process.env.REFRESH_JWT_SECRET_KEY,{
-        expiresIn:process.env.REFRESH_JWT_EXPIRES_IN 
-    })
-}
