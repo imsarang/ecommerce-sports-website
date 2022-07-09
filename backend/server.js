9aabd77d83ce27ba3f907e39333b579cc9100d3c
imsarang <84568747+imsarang@users.noreply.github.com>
2022-07-09 23:08:15 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 // including .env file
+const cors = require('cors')
+const passport = require('passport')
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const flash = require('connect-flash')
+const path = require('path')
+
 const dotenv = require("dotenv")
 dotenv.config({path:'./det.env'})
-
-const cors = require('cors')
-
+// const passportSetup = require('./passport')
 
 const connectDB = require('./db/connect')
 
@@ -14,30 +18,52 @@ const userRoutes = require("./routes/userRoutes")
 const productRoutes = require("./routes/productRoutes")
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const cookieParser = require('cookie-parser')
-
+const expressSession = require('express-session')
 require('./controller/passport')
+
+// port
+const port = process.env.PORT || 5000
 
 // connect to database
 connectDB()
 
 //middleware to handle json data and payloads
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods:"GET,POST,PUT,DELETE",
+    credentials:true,
+}))
 
 app.use(express.json())
 
 // middleware to handle cookie
 app.use(cookieParser('EXPRESS_SESSION_SECRET'))
+// app.use(expressSession({
+//     secret:process.env.EXPRESS_SESSION_SECRET,
+//     resave:true,
+//     saveUninitialized:false,
 
+// }))
+
+// user routes
 app.use("/api/v1",userRoutes)
 
 // product routes
 app.use("/api/v2",productRoutes)
 
 // middleware routes
-// app.use(notFound)
+app.use(notFound)
 app.use(errorHandler)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/sports/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(
+            __dirname,'frontend/sports','build','index.html'
+        ))
+    })
+}
 
-app.listen(process.env.PORT || 5000,()=>{
-    console.log(`Server is running on port : ${process.env.PORT || 5000}`);
+app.listen(port,()=>{
+    console.log(`Server is running on port : ${port}`);
 })
